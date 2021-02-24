096452226c0f03e88bc6156a4783e7e15dde4906
serNAVARRO7 <sernavarrops3@gmail.com>
2021-02-24 09:42:16 +0100

    Refactor ch06

--- exercises/ch06/main.js
+++ exercises/ch06/main.js
@@ -19,12 +19,12 @@ require(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
 
   const img = src => $('<img />', { src });
   const mediaUrl = compose(prop('m'), prop('media'));
-  const mediaUrls = compose(map(mediaUrl), prop('items'));
-  const images = compose(map(img), mediaUrls);
+  const mediaToImg = compose(img, mediaUrl);
+  const images = compose(map(mediaToImg), prop('items'));
 
   // -- Impure ---------------------------------------------------------
   const render = compose(Impure.setHtml('#js-main'), images);
   const app = compose(Impure.getJSON(render), url);
 
-  app('cats');
+  app('basketball');
 });
