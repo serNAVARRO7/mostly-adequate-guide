a382edc185f694ee0c356df57ea7b41b7b04927e
serNAVARRO7 <sernavarrops3@gmail.com>
2021-02-25 09:36:13 +0100

    ch07 Hindley-Milner

---
+++ exercises/ch07/examples.js
@@ -0,0 +1,23 @@
+// strLength :: String -> Number
+const strLength = s => s.length;
+
+// join :: String -> [String] -> String
+const join = curry((what, xs) => xs.join(what));
+
+// match :: Regex -> String -> [String]
+const match = curry((reg, s) => s.match(reg));
+
+// replace :: Regex -> String -> String -> String
+const replace = curry((reg, sub, s) => s.replace(reg, sub));
+
+// replace :: Regex -> (String -> (String -> String))
+const replace = curry((reg, sub, s) => s.replace(reg, sub));
+
+// head :: [a] -> a
+const head = xs => xs[0];
+
+// filter :: (a -> Bool) -> [a] -> [a]
+const filter = curry((f, xs) => xs.filter(f));
+
+// reduce :: ((b, a) -> b) -> b -> [a] -> b
+const reduce = curry((f, x, xs) => xs.reduce(f, x));
